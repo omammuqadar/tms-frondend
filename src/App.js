8e46c1bc937a1d7f86c3fde7b4a30e8e703b570a
Abu Bakar <abubakar0978123@gmail.com>
2023-03-21 13:10:41 +0500

    animated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { useState, useEffect  } from 'react';
+import {  useEffect  } from 'react';
 // external 
 import { Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // internal
 import './style/index.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
  function App(){  
+  useEffect(()=>{
+    // its a animation of js 
+    Aos.init();
+
+  },[])
 
   return (
     <div className="app">
